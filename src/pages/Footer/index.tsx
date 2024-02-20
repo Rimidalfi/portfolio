abdf16d9ee1e5d6be3946f89599a7969e5f25423
Wladimir Janowitsch <rimidalf@tutamail.com>
2024-02-20 14:44:53 +0100

    Socials implementation

--- src/pages/Footer/index.tsx
+++ src/pages/Footer/index.tsx
@@ -1,7 +1,47 @@
 import path from "../../routes/pathConstants";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+// import { createClient } from "contentful";
+import Social from "./Social";
+import getSocials from "./getSocials";
+
+interface SocialURLS {
+  logoURL: string;
+  URL: string;
+}
+interface Socials {
+  [key: string]: SocialURLS;
+}
 
 function Footer() {
+  const { VITE_ACCESS_TOKEN, VITE_SPACE_ID } = import.meta.env;
+  const [socials, setSocials] = useState<Socials>({});
+  useEffect(() => {
+    getSocials(VITE_SPACE_ID, VITE_ACCESS_TOKEN, setSocials, socials);
+    // const client = createClient({
+    //   space: VITE_SPACE_ID,
+    //   environment: "master",
+    //   accessToken: VITE_ACCESS_TOKEN,
+    // });
+    // client
+    //   .getEntries({
+    //     content_type: "socials",
+    //   })
+    //   .then((entry: any) => {
+    //     const updatedSocials: Socials = { ...socials };
+    //     const items: any[] = entry.items;
+    //     console.log(items);
+    //     items.forEach((element) => {
+    //       const title: string = element.fields.logoTitle;
+    //       updatedSocials[title] = {
+    //         logoURL: element.fields.logoGraphics[0].fields.file.url,
+    //         URL: element.fields.logoLink,
+    //       };
+    //     });
+    //     setSocials(updatedSocials);
+    //   })
+    //   .catch((err) => console.error("ERROR:", err));
+  }, []);
   return (
     <footer className="flex justify-evenly p-16 bg-neutral-700 text-white leading-8">
       <ul className="pr-8">
@@ -20,8 +60,16 @@ function Footer() {
       </ul>
       <div>Quote</div>
       <ul className="pl-8">
-        <li>github</li>
-        <li>linkedIn</li>
+        <li>
+          {socials.Github ? <Social socialEntry={socials.Github} /> : "loading"}
+        </li>
+        <li className="pt-4">
+          {socials.LinkedIn ? (
+            <Social socialEntry={socials.LinkedIn} />
+          ) : (
+            "loading"
+          )}
+        </li>
       </ul>
     </footer>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
+    fontFamily:{
+      sans: ["Montserrat-Regular","sans-serif"],
+      "montserrat-italic":["Montserrat-Italic","sans-serif"],
+      "montserrat-bold":["Montserrat-Bold","sans-serif"],
+      "montserrat-bold-italic":["Montserrat-BoldItalic","sans-serif"],
+      "montserrat-thin":["Montserrat-Thin","sans-serif"],
+      "montserrat-thin-italic":["Montserrat-ThinItalic","sans-serif"],
+      "shrikhand":["Shrikhand","serif"],
+  },
     extend: {},
   },
   plugins: [],
