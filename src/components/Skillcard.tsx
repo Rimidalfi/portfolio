7ac0d9e1d66b5162d160d732fa55bfb6e3d3deb1
Wladimir Janowitsch <rimidalf@tutamail.com>
2024-04-12 15:59:03 +0200

    skillset contetnful fetch

--- src/components/Skillcard.tsx
+++ src/components/Skillcard.tsx
@@ -1,22 +1,31 @@
+import { useState, useEffect } from "react";
+import getSkills from "../utils/getSkills";
+
 interface skillProps {
   skill: string;
   size: string;
 }
 
+export interface SkillData {
+  skill: string;
+  level: string;
+  img: string | null;
+}
+
 function SingleSkill({ skill, size }: skillProps) {
   let tailwindInstruction = "";
   switch (size) {
-    case "s":
+    case "1":
       tailwindInstruction =
-        "text-xs inline-block px-6 py-3 text-center text-white align-middle transition-all rounded-full cursor-pointer leading-pro ease-soft-in tracking-tight-soft shadow-soft-md hover:scale-110 hover:rotate-2 active:opacity-85 drop-shadow-xl hover:shadow-lg hover:bg-zinc-900 hover:shadow-blue-500/25";
+        "text-xs inline-block px-6 py-3 text-center text-white align-middle transition-all rounded-full cursor-pointer leading-pro ease-soft-in tracking-tight-soft shadow-soft-md drop-shadow-xl hover:scale-110  hover:rotate-2 active:opacity-85  hover:shadow-lg hover:bg-zinc-900 hover:shadow-blue-500/25";
       break;
-    case "m":
+    case "2":
       tailwindInstruction =
-        "text-xl inline-block px-6 py-3 text-center text-white align-middle transition-all rounded-full cursor-pointer leading-pro ease-soft-in tracking-tight-soft shadow-soft-md hover:scale-110 hover:rotate-2 active:opacity-85 drop-shadow-xl hover:shadow-lg hover:bg-zinc-900 hover:shadow-indigo-500/25";
+        "text-xl inline-block px-6 py-3 text-center text-white align-middle transition-all rounded-full cursor-pointer leading-pro ease-soft-in tracking-tight-soft shadow-soft-md drop-shadow-xl hover:scale-110 hover:rotate-2 active:opacity-85  hover:shadow-lg hover:bg-zinc-900 hover:shadow-indigo-500/25";
       break;
-    case "l":
+    case "3":
       tailwindInstruction =
-        "text-4xl inline-block px-6 py-3 text-center text-white align-middle transition-all rounded-full cursor-pointer leading-pro ease-soft-in tracking-tight-soft shadow-soft-md hover:scale-110 hover:rotate-[-3deg] active:opacity-85 drop-shadow-xl hover:shadow-lg hover:bg-zinc-900 hover:shadow-purple-500/25";
+        "text-4xl inline-block px-6 py-3 text-center text-white align-middle transition-all rounded-full cursor-pointer leading-pro ease-soft-in tracking-tight-soft shadow-soft-md drop-shadow-xl hover:scale-110 hover:rotate-[-3deg] active:opacity-85  hover:shadow-lg hover:bg-zinc-900 hover:shadow-purple-500/25";
       break;
   }
 
@@ -24,14 +33,27 @@ function SingleSkill({ skill, size }: skillProps) {
 }
 
 export default function Skillcard() {
+  const [skillData, setSkillData] = useState<SkillData[]>([]);
+  useEffect(() => {
+    getSkills(setSkillData);
+  }, []);
+  const skillEntries = skillData.map((entry) => {
+    return (
+      <div key={`${entry.skill}_${entry.level}`}>
+        <SingleSkill skill={entry.skill} size={entry.level} />
+      </div>
+    );
+  });
   return (
-    <div className="flex flex-col justify-center items-center h-52 md:h-80 m-4 w-screen bg-gradient-to-tr from-zinc-900 to-zinc-700 ">
-      <div className=" w-10/12 md:w-8/12 lg:w-6/12">
-        <SingleSkill skill={"Typescript"} size={"s"} />
-        <SingleSkill skill={"Docker"} size={"s"} />
-        <SingleSkill skill={"Python"} size={"l"} />
-        <SingleSkill skill={"Photoshop"} size={"s"} />
-        <SingleSkill skill={"HTML"} size={"m"} />
+    <div className="flex flex-wrap justify-center items-center min:h-52 md:min:h-80 m-4 w-screen bg-gradient-to-tr from-zinc-900 to-zinc-700 ">
+      <div className="w-screen bg-white z-10 p-6 md:px-14 font-montserrat-bold shadow-xl shadow-zinc-800">
+        <h3 className="text-xl font-montserrat-bold">Skills</h3>
+      </div>
+      <div className="my-4 w-10/12 md:w-8/12 lg:w-6/12 flex flex-wrap justify-center items-center z-0">
+        {skillEntries}
+        <button className=" inline-block px-6 py-3 text-center text-white align-middle transition-all rounded-full cursor-pointer leading-pro ease-soft-in tracking-tight-soft shadow-soft-md drop-shadow-xl hover:scale-110 focus:scale-110 focus:rotate-2 hover:rotate-2 active:opacity-85 focus:shadow-lg focus:bg-zinc-900 focus:shadow-blue-500/25 hover:shadow-lg hover:bg-zinc-900 hover:shadow-blue-500/25">
+          test
+        </button>
       </div>
     </div>
   );
