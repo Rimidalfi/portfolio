4b0047ba383774b468def5cfa6b0fb6eaeff7d44
Wladimir Janowitsch <rimidalf@tutamail.com>
2024-09-21 09:36:36 +0200

    nginx api proxy

--- src/utils/getLogo.tsx
+++ src/utils/getLogo.tsx
@@ -1,29 +1,14 @@
 import { SetStateAction, Dispatch } from "react";
-import getEntry from "./getEntry";
-// import client from "../utils/client";
+// import getEntry from "./getEntry";
+import client from "../utils/client";
 const logoEntry: string = "28Hl7k83mhlCbExKUHj2Fs";
-
-// function getLogo(
-//   setLogo: Dispatch<SetStateAction<string>>,
-//   index: number
-// ): void {
-//   client
-//     .getEntry(logoEntry)
-//     .then((entry: any) => {
-//       const logoArray: any[] = entry.fields.logoGraphics;
-//       const logoUrl: string = logoArray[index].fields.file.url;
-//       setLogo(logoUrl);
-//     })
-//     .catch((err) => console.error("ERROR:", err));
-// }
-
-// export default getLogo;
 
 function getLogo(
   setLogo: Dispatch<SetStateAction<string>>,
   index: number
 ): void {
-  getEntry(logoEntry)
+  client
+    .getEntry(logoEntry)
     .then((entry: any) => {
       const logoArray: any[] = entry.fields.logoGraphics;
       const logoUrl: string = logoArray[index].fields.file.url;
@@ -33,3 +18,18 @@ function getLogo(
 }
 
 export default getLogo;
+
+// function getLogo(
+//   setLogo: Dispatch<SetStateAction<string>>,
+//   index: number
+// ): void {
+//   getEntry(logoEntry)
+//     .then((entry: any) => {
+//       const logoArray: any[] = entry.fields.logoGraphics;
+//       const logoUrl: string = logoArray[index].fields.file.url;
+//       setLogo(logoUrl);
+//     })
+//     .catch((err) => console.error("ERROR:", err));
+// }
+
+// export default getLogo;
