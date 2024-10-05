514c70d1fe77dd170974d7a25e67518b14e20b8d
Wladimir Janowitsch <rimidalf@tutamail.com>
2024-10-05 13:06:51 +0200

    Nginx API Call implemented for Logo

--- src/utils/getEntry.tsx
+++ src/utils/getEntry.tsx
@@ -1,8 +1,15 @@
-function getEntry(url: string) {
+function getEntry(url: string): Promise<Record<string, unknown>> {
   const resquest: string =
     "https://wladimir.janowitsch.com/api/contentful/entry/";
 
-  return fetch(resquest + url);
+  return fetch(resquest + url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Response NOT OK! 🥲");
+      }
+      return response.json();
+    })
+    .catch((err) => console.error("GET ENTRY ERROR:", err));
 }
 
 export default getEntry;
--- src/utils/getLogo.tsx
+++ src/utils/getLogo.tsx
@@ -1,8 +1,23 @@
 import { SetStateAction, Dispatch } from "react";
 import getEntry from "./getEntry";
-// import client from "../utils/client";
 const logoEntry: string = "28Hl7k83mhlCbExKUHj2Fs";
 
+function getLogo(
+  setLogo: Dispatch<SetStateAction<string>>,
+  index: number
+): void {
+  getEntry(logoEntry)
+    .then((json: any) => {
+      const logoArray: any[] = json.includes.Asset;
+      const logoUrl: string = logoArray[index].fields.file.url;
+      setLogo(logoUrl);
+    })
+    .catch((err) => console.error("ERROR:", err));
+}
+
+export default getLogo;
+
+// import client from "../utils/client";
 // function getLogo(
 //   setLogo: Dispatch<SetStateAction<string>>,
 //   index: number
@@ -16,20 +31,4 @@ const logoEntry: string = "28Hl7k83mhlCbExKUHj2Fs";
 //     })
 //     .catch((err) => console.error("ERROR:", err));
 // }
-
 // export default getLogo;
-
-function getLogo(
-  setLogo: Dispatch<SetStateAction<string>>,
-  index: number
-): void {
-  getEntry(logoEntry)
-    .then((entry: any) => {
-      const logoArray: any[] = entry.fields.logoGraphics;
-      const logoUrl: string = logoArray[index].fields.file.url;
-      setLogo(logoUrl);
-    })
-    .catch((err) => console.error("ERROR:", err));
-}
-
-export default getLogo;
