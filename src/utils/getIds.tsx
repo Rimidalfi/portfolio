9ac8869efe7b9cc2acc12eeaca8b3222e634aeb2
Wladimir Janowitsch <rimidalf@tutamail.com>
2024-10-06 23:25:20 +0200

    vita migration to proxy api consumption

--- src/utils/getIds.tsx
+++ src/utils/getIds.tsx
@@ -1,15 +1,43 @@
 import { SetStateAction, Dispatch } from "react";
-import client from "../utils/client";
+import getEntries from "./getEntries";
+
 import { VitaObject } from "../components/VitaEntries";
+
+// export default function getIds(
+//   setIds: Dispatch<SetStateAction<VitaObject[]>>
+// ): void {
+//   client
+//     .getEntries({ content_type: "vita" })
+//     .then((entry: any) => {
+//       //console.log("getIds", entry.items);
+//       const entryIds = entry.items
+//         .map((item: any) => {
+//           if (item) {
+//             return {
+//               vitaId: item.sys.id,
+//               vitaStartDate: item.fields.vitaStartDate.slice(0, 4),
+//               vitaEndDate: item.fields.vitaEndDate.slice(0, 4),
+//             };
+//           }
+//         })
+//         .sort(
+//           (a: any, b: any) =>
+//             parseInt(b?.vitaEndDate) - parseInt(a?.vitaEndDate)
+//         );
+//       //console.log("Sorted list", entryIds);
+//       setIds(entryIds);
+//     })
+//     .catch((err) => console.error("getIds ERROR:", err));
+// }
 
 export default function getIds(
   setIds: Dispatch<SetStateAction<VitaObject[]>>
 ): void {
-  client
-    .getEntries({ content_type: "vita" })
-    .then((entry: any) => {
-      //console.log("getIds", entry.items);
-      const entryIds = entry.items
+  getEntries("vita")
+    .then((json: any) => {
+      console.log("VITA JSON 🤗", json);
+
+      const entryIds = json.items
         .map((item: any) => {
           if (item) {
             return {
@@ -23,7 +51,6 @@ export default function getIds(
           (a: any, b: any) =>
             parseInt(b?.vitaEndDate) - parseInt(a?.vitaEndDate)
         );
-      //console.log("Sorted list", entryIds);
       setIds(entryIds);
     })
     .catch((err) => console.error("getIds ERROR:", err));
